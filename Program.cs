3dfc94e8a2c5ecc946939514d20394cfb4fa8019
LogFlames <elilun03@gmail.com>
2022-02-13 22:17:34 +0100

    Initial vs2019 project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stract_lang
+{
+    class StractLang
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
